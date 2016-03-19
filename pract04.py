44eb5fce305ab22f5f2674cc2471aa22fca4d5e5
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-03-19 11:07:12 +0000

    Added multiplayer functionally

---
+++ pract04.py
@@ -0,0 +1,51 @@
+import games
+
+game = games.ConnectFour()
+state = game.initial
+
+player = 'X'
+
+columns = [6, 6, 6, 6, 6, 6]
+mode = raw_input("1: Multiplayer, 2: vs CPU")
+
+
+def check_legal_move(y):
+    while y < 0 or y > 5:
+        y = int(raw_input("Movimiento invalido, por favor vuelva a intentarlo"))
+    return y
+
+
+def player_move():
+    global column, y, x, state, player
+    print_board(state)
+    column = raw_input("Elija la columna en la que desea jugar (0-5)")
+    y = check_legal_move(int(column))
+    x = columns[y]
+    state = game.make_move((x, y), state)
+    columns[y] -= 1
+    if player == 'X':
+        player = 'O'
+    else:
+        player = 'X'
+
+
+def print_board(state):
+    game.display(state)
+    print "Jugador a mover:", game.to_move(state)
+
+
+while True:
+    if int(mode) == 1:
+        player_move()
+        print "-------------------"
+    if int(mode) == 2:
+        if player == 'X':
+            player_move()
+        else:
+            # maquina
+            print ()
+        print "-------------------"
+    if game.terminal_test(state):
+        game.display(state)
+        print "Final de la partida"
+        break
---
+++ prueba.py
@@ -0,0 +1,2 @@
+moves = [(6, y) for y in range(0, 5)]
+print moves
