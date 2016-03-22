bad3b05b1bc85c6a2e015c73226f2e8558e6f0f8
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-03-22 18:40:40 +0000

    Visual_view refactored and added game-over feature

--- visual_view.py
+++ visual_view.py
@@ -7,6 +7,7 @@ state = game.initial
 
 player = 'X'
 player2_log = ""
+credit_flag = True
 player_log = "Comienza el juego!"
 cpu_log = ""
 black = (0, 0, 0)
@@ -40,22 +41,9 @@ fuente = pygame.font.Font(None, 36)
 show_intro = True
 intro_page = 1
 
-while not done and show_intro:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            done = True
-        if evento.type == pygame.MOUSEBUTTONDOWN:
-            intro_page += 1
-            if intro_page == 3:
-                show_intro = False
-        if evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_m:
-                mode += 1
-                if mode == 5:
-                    mode = 1
 
-    pantalla.fill(black)
-
+def display_intro():
+    global texto, difficult
     if intro_page == 1:
         texto = fuente.render("4 en raya", True, white)
         pantalla.blit(texto, [10, 10])
@@ -67,7 +55,6 @@ while not done and show_intro:
         pantalla.blit(texto, [400, 445])
         texto = fuente.render("Stefan Hautz", True, white)
         pantalla.blit(texto, [400, 470])
-
     if intro_page == 2:
         texto = fuente.render("Pulse m para cambiar la modalidad", True, white)
         pantalla.blit(texto, [10, 10])
@@ -95,48 +82,38 @@ while not done and show_intro:
             difficult = 10
             pantalla.blit(texto, [10, 120])
 
-    reloj.tick(20)
 
-    pygame.display.flip()
+def display_igtexts():
+    global texto
+    texto = fuente.render("Pulsa 'H' para obtener una pista", True, white)
+    pantalla.blit(texto, [0, 180])
+    texto = fuente.render("Pistas restantes: " + str(hints), True, red)
+    pantalla.blit(texto, [0, 210])
+    texto = fuente.render(player_log, True, blue)
+    pantalla.blit(texto, [160, 0])
+    texto = fuente.render(player2_log, True, red)
+    pantalla.blit(texto, [160, 25])
+    texto = fuente.render(cpu_log, True, red)
+    pantalla.blit(texto, [160, 20])
+    texto = fuente.render("Turno del jugador '" + str(player) + "'", True, white)
+    pantalla.blit(texto, [0, 470])
+    if hint_flag == 1:
+        texto = fuente.render("Has probado a mover en la posicion " + str(hint_move) + " ?", True, white)
+        pantalla.blit(texto, [0, 235])
 
-while not done:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            hecho = True
-        elif evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_h:
-                if hints > 0:
-                    hint_move = games.alphabeta_search(state, game, d=6, cutoff_test=None,
-                                                       eval_fn=heuristic.compute_utility(state))
-                    hints -= 1
-                    print hint_move
-                    hint_flag = 1
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            if mode == 1:
-                pos = pygame.mouse.get_pos()
-                y = pos[0] // (width + margin)
-                x = pos[1] // (height + margin)
-                if grid[x][y] == 0 and (x, y) in state.moves:
-                    state = game.make_move((x, y), state)
-                    if player == 'X':
-                        grid[x][y] = 1
-                        player = 'O'
-                        player_log = "El jugador '"+str(player)+"' ha movido en la posicion: " + str((x, y))
-                    else:
-                        grid[x][y] = 2
-                        player = 'X'
-                        player2_log = "El jugador '"+str(player)+"' ha movido en la posicion: " + str((x, y))
-            else:
-                if player == 'X':
-                    pos = pygame.mouse.get_pos()
-                    y = pos[0] // (width + margin)
-                    x = pos[1] // (height + margin)
-                    if grid[x][y] == 0 and (x, y) in state.moves:
-                        state = game.make_move((x, y), state)
-                        grid[x][y] = 1
-                        player = 'O'
-                        player_log = "El jugador ha movido en la posicion: " + str((x, y))
-    pantalla.fill(black)
+
+def cpu_play():
+    global state, cpu_log, player
+    move = games.alphabeta_search(state, game, d=difficult, cutoff_test=None,
+                                  eval_fn=heuristic.compute_utility(state))
+    state = game.make_move(move, state)
+    grid[move[0]][move[1]] = 2
+    cpu_log = "La CPU ha movido en la posicion" + str(move)
+    player = 'X'
+
+
+def display_board():
+    global x, y
     for x in range(7):
         for y in range(6):
             color = white
@@ -150,34 +127,95 @@ while not done:
                               (margin + height) * x + margin,
                               width,
                               height])
-    texto = fuente.render("Pulsa 'H' para obtener una pista", True, white)
-    pantalla.blit(texto, [0, 180])
-    texto = fuente.render("Pistas restantes: " + str(hints), True, red)
-    pantalla.blit(texto, [0, 210])
-    texto = fuente.render(player_log, True, blue)
-    pantalla.blit(texto, [160, 0])
-    texto = fuente.render(player2_log, True, red)
-    pantalla.blit(texto, [160, 25])
-    texto = fuente.render(cpu_log, True, red)
-    pantalla.blit(texto, [160, 20])
-    texto = fuente.render("Turno del jugador '" + str(player) + "'", True, white)
-    pantalla.blit(texto, [0, 470])
 
-    if hint_flag == 1:
-        texto = fuente.render("Has probado a mover en la posicion " + str(hint_move) + " ?", True, white)
-        pantalla.blit(texto, [0, 235])
+
+def single_player():
+    global pos, y, x, state, player, player_log, player2_log
+    pos = pygame.mouse.get_pos()
+    y = pos[0] // (width + margin)
+    x = pos[1] // (height + margin)
+    if grid[x][y] == 0 and (x, y) in state.moves:
+        state = game.make_move((x, y), state)
+        if player == 'X':
+            grid[x][y] = 1
+            player = 'O'
+            player_log = "El jugador '" + str(player) + "' ha movido en la posicion: " + str((x, y))
+        else:
+            grid[x][y] = 2
+            player = 'X'
+            player2_log = "El jugador '" + str(player) + "' ha movido en la posicion: " + str((x, y))
+
+
+def player_play():
+    global pos, y, x, state, player, player_log
+    if player == 'X':
+        pos = pygame.mouse.get_pos()
+        y = pos[0] // (width + margin)
+        x = pos[1] // (height + margin)
+        if grid[x][y] == 0 and (x, y) in state.moves:
+            state = game.make_move((x, y), state)
+            grid[x][y] = 1
+            player = 'O'
+            player_log = "El jugador ha movido en la posicion: " + str((x, y))
+
+
+while not done and show_intro:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            done = True
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            intro_page += 1
+            if intro_page == 3:
+                show_intro = False
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_m:
+                mode += 1
+                if mode == 5:
+                    mode = 1
+
+    pantalla.fill(black)
+    display_intro()
+    reloj.tick(20)
+    pygame.display.flip()
+
+while not done:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            hecho = True
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_h:
+                if hints > 0:
+                    hint_move = games.alphabeta_search(state, game, d=6, cutoff_test=None,
+                                                       eval_fn=heuristic.compute_utility(state))
+                    hints -= 1
+                    hint_flag = 1
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if mode == 1:
+                single_player()
+            else:
+                player_play()
+    pantalla.fill(black)
+    display_board()
+    display_igtexts()
     pygame.display.flip()
     if mode != 1 and player == 'O':
-        move = games.alphabeta_search(state, game, d=difficult, cutoff_test=None,
-                                      eval_fn=heuristic.compute_utility(state))
-        state = game.make_move(move, state)
-        grid[move[0]][move[1]] = 2
-        cpu_log = "La CPU ha movido en la posicion" + str(move)
-        player = 'X'
+        cpu_play()
     if game.terminal_test(state):
         print "Final de la partida"
-        break
-
-    reloj.tick(20)
+        print "Ganador: " + player
+        done = True
+while credit_flag:
+    for evento in pygame.event.get():
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_ESCAPE:
+                credit_flag = False
+    pantalla.fill(black)
+    texto = fuente.render("Game Over", True, white)
+    texto_rect = texto.get_rect()
+    texto_x = pantalla.get_width() / 2 - texto_rect.width / 2
+    texto_y = pantalla.get_height() / 2 - texto_rect.height / 2
+    pantalla.blit(texto, [texto_x, texto_y])
+    pygame.display.flip()
+reloj.tick(20)
 
 pygame.quit()
