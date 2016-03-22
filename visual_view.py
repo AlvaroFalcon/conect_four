37cd1349b8942f0b67c7c62e773d9c97283dc6f7
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-03-22 17:28:09 +0000

    Added move logs and turn display

--- visual_view.py
+++ visual_view.py
@@ -6,7 +6,9 @@ game = games.ConnectFour()
 state = game.initial
 
 player = 'X'
-
+player2_log = ""
+player_log = "Comienza el juego!"
+cpu_log = ""
 black = (0, 0, 0)
 white = (255, 255, 255)
 blue = (0, 0, 255)
@@ -14,7 +16,7 @@ red = (255, 0, 0)
 width = 20
 height = 20
 margin = 5
-hintflag = 0
+hint_flag = 0
 grid = []
 for x in range(7):
     grid.append([])
@@ -108,7 +110,7 @@ while not done:
                                                        eval_fn=heuristic.compute_utility(state))
                     hints -= 1
                     print hint_move
-                    hintflag = 1
+                    hint_flag = 1
         elif evento.type == pygame.MOUSEBUTTONDOWN:
             if mode == 1:
                 pos = pygame.mouse.get_pos()
@@ -119,10 +121,12 @@ while not done:
                     if player == 'X':
                         grid[x][y] = 1
                         player = 'O'
+                        player_log = "El jugador '"+str(player)+"' ha movido en la posicion: " + str((x, y))
                     else:
                         grid[x][y] = 2
                         player = 'X'
-            elif mode == 2:
+                        player2_log = "El jugador '"+str(player)+"' ha movido en la posicion: " + str((x, y))
+            else:
                 if player == 'X':
                     pos = pygame.mouse.get_pos()
                     y = pos[0] // (width + margin)
@@ -131,6 +135,7 @@ while not done:
                         state = game.make_move((x, y), state)
                         grid[x][y] = 1
                         player = 'O'
+                        player_log = "El jugador ha movido en la posicion: " + str((x, y))
     pantalla.fill(black)
     for x in range(7):
         for y in range(6):
@@ -149,8 +154,17 @@ while not done:
     pantalla.blit(texto, [0, 180])
     texto = fuente.render("Pistas restantes: " + str(hints), True, red)
     pantalla.blit(texto, [0, 210])
-    if hintflag == 1:
-        texto = fuente.render("Has probado a mover en la posicion "+str(hint_move)+" ?", True, white)
+    texto = fuente.render(player_log, True, blue)
+    pantalla.blit(texto, [160, 0])
+    texto = fuente.render(player2_log, True, red)
+    pantalla.blit(texto, [160, 25])
+    texto = fuente.render(cpu_log, True, red)
+    pantalla.blit(texto, [160, 20])
+    texto = fuente.render("Turno del jugador '" + str(player) + "'", True, white)
+    pantalla.blit(texto, [0, 470])
+
+    if hint_flag == 1:
+        texto = fuente.render("Has probado a mover en la posicion " + str(hint_move) + " ?", True, white)
         pantalla.blit(texto, [0, 235])
     pygame.display.flip()
     if mode != 1 and player == 'O':
@@ -158,6 +172,7 @@ while not done:
                                       eval_fn=heuristic.compute_utility(state))
         state = game.make_move(move, state)
         grid[move[0]][move[1]] = 2
+        cpu_log = "La CPU ha movido en la posicion" + str(move)
         player = 'X'
     if game.terminal_test(state):
         print "Final de la partida"
